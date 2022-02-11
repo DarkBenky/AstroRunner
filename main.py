230b4eca682d5e3f2edfd29835644fd2f34596b1
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-02-11 11:16:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+from turtle import width
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+width = 1000
+height = 1000
+
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Astro Runner")
+
+# game variables
+running = True
+tile_size = 50
+
+world_data = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+# load images
+space = pygame.image.load("bg/space.png").convert_alpha()
+space = pygame.transform.scale(space,(1000,1000))
+
+# classes
+
+class World():
+    def __init__(self,data):
+        # variables
+        row_count = 0
+        self.tile_list = []
+        
+        # load images
+        block = pygame.image.load("Osticle/block.png").convert_alpha()
+        block_lava = pygame.image.load("Osticle/block_lava.png").convert_alpha()
+        spike = pygame.image.load("Osticle/Spike.png").convert_alpha()
+        
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(block,(tile_size,tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(block_lava,(tile_size,tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    img = pygame.transform.scale(spike,(tile_size,tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+    
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+                 
+world = World(world_data)     
+   
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    # draw background
+    screen.blit(space,(0,0))
+    
+    world.draw()
+    pygame.display.update()
+    
+pygame.quit()
